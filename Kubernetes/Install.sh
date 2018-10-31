33d1a27765d1d3b51af15f037e81a08b3696c271
cskarthik22 <38231831+cskarthik22@users.noreply.github.com>
2018-10-31 16:02:54 -0400

    Update Install.sh

--- Kubernetes/Install.sh
+++ Kubernetes/Install.sh
@@ -89,6 +89,9 @@ strace -eopenat kubectl version
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
+** WEAVE uses a protocol called Gossip to identify peer nodes, it uses in-memory datastore
+
+
 
 vim /etc/sysctl.conf
 net.ipv4.ip_forward = 1
